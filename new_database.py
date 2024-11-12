12fc02bf82c0dadbbeb0aa90fe29c9813908fa53
KazymaevSergey <intekt83@gmail.com>
2024-11-12 11:06:39 +0300

    Update new_database.py
    change connect

--- new_database.py
+++ new_database.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from sqlalchemy import text
+from sqlalchemy import create_engine
 
 
         
         
 class Database():
     def __init__(self):
-        self.conn=st.connection('tidb', type='sql')
+          
+        self.conn=create_engine(st.secrets.databaseTDIB.url)               #st.connection('tidb', type='sql')
         
         self.columns_dementia = '''
 age
@@ -28,29 +30,29 @@ assessment_prediction'''.split()
         
         
     def insert_data_dementia(self, data):
-        connection=self.conn
+        engine=self.conn
         
         dict_data=dict(zip(self.columns_dementia,data))
                
-        with connection.session as session:
-            session.execute(text( '''INSERT INTO ace_dementia
+        with engine.connect() as connect:
+                connect.execute(text( '''INSERT INTO ace_dementia
 (age, m_ACE, ACE, attention, memory, fluency, speech, spatials, 
 diagnosis, predicted_diagnosis, predicted_stepen, assessment_prediction)
             
 VALUES(
 :age, :m_ACE, :ACE, :attention, :memory, :fluency, :speech, :spatials, 
 :diagnosis, :predicted_diagnosis, :predicted_stepen, :assessment_prediction) ''' ), dict_data)
-            session.commit()
+                connect.commit()
     
     
     def insert_data(self, data):
-        connection=self.conn
+        engine=self.conn
         
         dict_data=dict(zip(self.columns, data))
        
         
-        with connection.session as session:
-            session.execute(text( '''INSERT INTO ace_general (      
+        with engine.connect() as connect:
+                connect.execute(text( '''INSERT INTO ace_general (      
             age, m_ACE, ACE, attantion, memory, fluence, speech, spatials, 
             stepen, damage, diagnos, assment_stepen, assment_damage)
                                   
@@ -58,8 +60,7 @@ VALUES(
             :age, :m_ACE, :ACE, :attantion, :memory, :fluence, :speech, :spatials,
             :stepen, :damage, :diagnos, :assment_stepen, :assment_damage)'''
                 ),  dict_data)
-    
-            session.commit()
+                connect.commit()
 
     
 
