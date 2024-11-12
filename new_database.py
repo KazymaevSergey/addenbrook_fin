6ad116c438b4181da7fa08b6f20554fe3814ea2b
KazymaevSergey <intekt83@gmail.com>
2024-11-12 19:32:04 +0300

    new file:   __pycache__/anomal_predict.cpython-311.pyc 	new file:   __pycache__/inputs_data.cpython-311.pyc 	new file:   __pycache__/interpetator.cpython-311.pyc 	new file:   __pycache__/new_database.cpython-311.pyc 	new file:   __pycache__/predict_clf.cpython-311.pyc 	new file:   __pycache__/predict_pytorch.cpython-311.pyc 	modified:   list_pages/predict_diagnos.py 	modified:   new_database.py 	modified:   packages.txt 	modified:   requirements.txt

--- new_database.py
+++ new_database.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from sqlalchemy import text
 
+import datetime
+
 
         
         
 class Database():
     def __init__(self):
+          
         self.conn=st.connection('tidb', type='sql')
         
         self.columns_dementia = '''
@@ -28,29 +31,32 @@ assessment_prediction'''.split()
         
         
     def insert_data_dementia(self, data):
-        connection=self.conn
+        connect=self.conn
         
         dict_data=dict(zip(self.columns_dementia,data))
+        
                
-        with connection.session as session:
-            session.execute(text( '''INSERT INTO ace_dementia
+        with connect.session as s:
+                s.execute(text( '''INSERT INTO ace_dementia
 (age, m_ACE, ACE, attention, memory, fluency, speech, spatials, 
 diagnosis, predicted_diagnosis, predicted_stepen, assessment_prediction)
             
 VALUES(
 :age, :m_ACE, :ACE, :attention, :memory, :fluency, :speech, :spatials, 
 :diagnosis, :predicted_diagnosis, :predicted_stepen, :assessment_prediction) ''' ), dict_data)
-            session.commit()
+                s.commit()
+                s.close()
     
     
     def insert_data(self, data):
-        connection=self.conn
+        connect=self.conn
         
         dict_data=dict(zip(self.columns, data))
+        
        
         
-        with connection.session as session:
-            session.execute(text( '''INSERT INTO ace_general (      
+        with connect.session as s:
+                s.execute(text( '''INSERT INTO ace_general (      
             age, m_ACE, ACE, attantion, memory, fluence, speech, spatials, 
             stepen, damage, diagnos, assment_stepen, assment_damage)
                                   
@@ -58,15 +64,16 @@ VALUES(
             :age, :m_ACE, :ACE, :attantion, :memory, :fluence, :speech, :spatials,
             :stepen, :damage, :diagnos, :assment_stepen, :assment_damage)'''
                 ),  dict_data)
-    
-            session.commit()
+                s.commit()
+                s.close()
 
     
 
 
 
 
-
+#Database().insert_data([43,23,18,12,14,16,26,18,
+#                        'no', 'no', 'no', 'no', 'no'])
 
 
    
