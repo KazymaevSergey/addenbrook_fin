8b0eb3f69d60076c0491cdd6a5cc632ab1e0c530
KazymaevSergey <intekt83@gmail.com>
2024-11-12 15:48:29 +0300

    Update new_database.py
    neon database

--- new_database.py
+++ new_database.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 class Database():
     def __init__(self):
           
-        self.conn=create_engine(st.secrets.neonDB.url)               #st.connection('tidb', type='sql')
+        self.conn=st.connection('neon', type='sql')
         
         self.columns_dementia = '''
 age
@@ -30,12 +30,12 @@ assessment_prediction'''.split()
         
         
     def insert_data_dementia(self, data):
-        engine=self.conn
+        connect=self.conn
         
         dict_data=dict(zip(self.columns_dementia,data))
                
-        with engine.connect() as connect:
-                connect.execute(text( '''INSERT INTO ace_dementia
+        with connect.session as s:
+                s.execute(text( '''INSERT INTO ace_dementia
 (age, m_ACE, ACE, attention, memory, fluency, speech, spatials, 
 diagnosis, predicted_diagnosis, predicted_stepen, assessment_prediction)
             
@@ -46,13 +46,13 @@ VALUES(
     
     
     def insert_data(self, data):
-        engine=self.conn
+        connect=self.conn
         
         dict_data=dict(zip(self.columns, data))
        
         
-        with engine.connect() as connect:
-                connect.execute(text( '''INSERT INTO ace_general (      
+        with connect.session as s:
+                s.execute(text( '''INSERT INTO ace_general (      
             age, m_ACE, ACE, attantion, memory, fluence, speech, spatials, 
             stepen, damage, diagnos, assment_stepen, assment_damage)
                                   
